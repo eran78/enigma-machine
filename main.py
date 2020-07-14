ca4c84e83feddb1771ea1137f1e719297f90413b
eran78 <45491129+eran78@users.noreply.github.com>
2020-07-14 14:38:08 +0200

    Added user input (unfinished)

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ def alphabet():
   rotor2 = "mnopqrstuvwxyzabcdefghijkl"
   rotor3 = "stuvwxyzabcdefghijklmnopqr"
   
+alphabet()
+
+def user_input():
+
+  code = input()
+  return code
+
+user_input()
+
+def enigma_encrypt():
+
+  code = user_input()
