719afa717769d4efcfe872020981d712c4d88bb5
eran78 <45491129+eran78@users.noreply.github.com>
2020-07-14 14:36:21 +0200

    Alphabet list replaced with strings

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def enigma()
+def alphabet():
 
-  alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X"","Y","Z"]
-
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  rotor1 = "ghijklmnopqrstuvwxyzabcdef"
+  rotor2 = "mnopqrstuvwxyzabcdefghijkl"
+  rotor3 = "stuvwxyzabcdefghijklmnopqr"
   
