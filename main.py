5d379dcbe6c40f5af47517551a1bb263730bd833
eran78 <45491129+eran78@users.noreply.github.com>
2020-07-15 12:04:10 +0200

    Final overhaul, easier to read and more compact
    Though it's more compact, it is still not functional. As of now, July 2020, this is the final commit until the next time.

--- main.py
+++ main.py
@@ -1,111 +1,78 @@
 import string
 
-
-class Enigma:
-  alphabet = list(string.ascii_lowercase)
-  rotor1 = list("ghijklmnopqrstuvwxyzabcdef")
-  rotor2 = list("mnopqrstuvwxyzabcdefghijkl")
-  rotor3 = list("stuvwxyzabcdefghijklmnopqr")
-
-  def _init_(self, pos1, pos2, pos3):
-    self.rotate(self.rotor1, pos1)
-    self.rotate(self.rotor2, pos2)
-    self.rotate(self.rotor3, pos3)
-
-  def rotate(self, rotor, amount):
-    print(f"rotor {rotor} set on position {amount}")
-
-  def enigma_encrypt(self,letter):
-    idx = self.alphabet.index(letter)
-    new_letter = self.rotor1[idx]
-    print("letter : " + letter + "\tnew_letter: " + new_letter)
-    
-    idx = self.alphabet.index(new_letter)
-    new_letter = self.rotor2[idx]
-    print("letter : " + letter + "\tnew_letter: " + new_letter)
-
-    idx = self.alphabet.index(new_letter)
-    new_letter = self.rotor3[idx]
-    print("letter : " + letter + "\tnew_letter: " + new_letter)
-    
-    #idx = reversed(self.alphabet.index(letter))
-    #new_letter = self.rotor3[idx]
-    #print("letter : " + letter + "\tnew_letter: " +new_letter)
-      
-    #idx = reversed(self.alphabet.index(new_letter))
-    #new_letter = self.rotor2[idx]
-    #print("letter : " + letter + "\tnew_letter: " +new_letter)
-
-    #idx = reversed(self.alphabet.index(new_letter))
-    #new_letter = self.rotor1[idx]
-    #print("letter : " + letter + "\tnew_letter: " +new_letter)
-
-    reflector_1 = {'a':'n','b':'o','c':'p','d':'q','e':'r','f':'s','g':'t','h':'u','i':'v','j':'w','k':'x','l':'y','m':'z'}
-
-    reflector_2 = {'n':'a','o':'b','p':'c','q':'d','r':'e','s':'f','t':'g','u':'h','v':'i','w':'j','x':'k','y':'l','z':'m'}
-
-    try:
-      reflector_1[new_letter]
-    except:
-      try:
-        reflector_2[new_letter]
-      except:
-        print("This letter is not in the reflector")
-      else:
-        return reflector_2[new_letter]
-    else:
-      return reflector_1[new_letter]
-      
-    return new_letter
-    
-process = Enigma()
-
-
 def shift(text, amount):
   part1 = text[amount:26]
-  part2 = text[0:amount]
+  part2 = text[0:amount]  
   return part1 + part2
 
+class Enigma:
+  alphabet = string.ascii_lowercase
+  rotor1 = "ghijklmnopqrstuvwxyzabcdef"
+  rotor2 = "mnopqrstuvwxyzabcdefghijkl"
+  rotor3 = "stuvwxyzabcdefghijklmnopqr"
+  rotations = 0
 
-sentence = input('Enter sentence here:\n').lower()
-newSentence = ""
+  def __init__(self, pos1, pos2, pos3):
+    self.rotor1 = shift(self.rotor1, pos1)
+    self.rotor2 = shift(self.rotor2, pos2)
+    self.rotor3 = shift(self.rotor3, pos3)    
 
+  def encrypt(self, sentence):
 
-rotations = 0
-# elke letter encrypten
-for letter in sentence:
-  rotations += 1
-  # voor elke letter moet rotor1 één positie opschuiven
-  process.rotor1 = shift(process.rotor1, 1)
+    newSentence = ""
+    
+    for letter in sentence:
+      self.rotations += 1
 
-  # rotor twee schuift één positie naar op als rotor 1 een rondje heeft gemaakt 
-  if rotations % 26 == 0:
-    process.rotor2 = shift(process.rotor2, 1)
+      print(letter, self.rotor1)
 
-  # rotor ..
-  if rotations % (26 * 26) == 0:
-    process.rotor3 = shift(process.rotor3, 1)
+      # voor elke letter moet rotor1 één positie opschuiven
+      self.rotor1 = shift(self.rotor1, 1)
 
-  if letter == " ":
-    newSentence += " "
-    continue
-  encryptedLetter = process.enigma_encrypt(letter)
-  newSentence += encryptedLetter
+      print(letter, self.rotor1)
 
-for letter in sentence:
-  #makes sure that the rotors don't shift any further
-  process.rotor1 = shift(process.rotor1, -1)
+      # rotor twee schuift één positie naar op als rotor 1 een rondje heeft gemaakt 
+      if self.rotations % 26 == 0:
+        self.rotor2 = shift(self.rotor2, 1)
 
-  #likewise for rotor 2 and 3
-  if rotations % 26 == 0:
-    process.rotor2 = shift(process.rotor2, -1)
+      # rotor ..
+      if self.rotations % (26 * 26) == 0:
+        self.rotor3 = shift(self.rotor3, 1)
 
-  if rotations % (26*26) == 0:
-    process.rotor3 = shift(process.rotor3, -1)
-
-  #don't forget the spacings :D
-  if letter == " ":
-    newSentence += ""
-    continue
+      # haal letter door rotoren
+      newSentence += self.encrypt_letter(letter)
+    
+    return newSentence
   
-  print(newSentence)
+  def encrypt_letter(self,letter):
+    idx = self.alphabet.index(letter)
+    new_letter = self.rotor1[idx]    
+    
+    idx = self.alphabet.index(new_letter)
+    new_letter = self.rotor2[idx]    
+
+    idx = self.alphabet.index(new_letter)
+    new_letter = self.rotor3[idx]    
+
+    reflector_1 = {'a':'n','b':'o','c':'p','d':'q','e':'r','f':'s','g':'t','h':'u','i':'v','j':'w','k':'x','l':'y','m':'z','n':'a','o':'b','p':'c','q':'d','r':'e','s':'f','t':'g','u':'h','v':'i','w':'j','x':'k','y':'l','z':'m'}
+
+    # letter door reflector
+    newletter = reflector_1[new_letter]
+    
+    idx = self.alphabet.index(newletter)
+    new_letter = self.rotor3[idx]    
+    
+    idx = self.alphabet.index(new_letter)
+    new_letter = self.rotor2[idx]    
+
+    idx = self.alphabet.index(new_letter)
+    new_letter = self.rotor1[idx]
+    
+    return new_letter
+    
+
+machineA = Enigma(1,2,3)
+sentence = input('Enter sentence here:\n').lower()
+
+output = machineA.encrypt(sentence)
+print(output)
